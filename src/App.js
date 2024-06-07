ea0eaa7c1eeea6c986e962d0dda99f9bb07c5089
Vishal Kumar <iamvishukr@gmail.com>
2024-06-07 23:14:54 +0530

    margin bottom for small devices

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
-      <div className="container mx-auto px-2 lg:mb-6 mb-32">
+      <div className="container mx-auto px-2 lg:mb-6 mb-24">
         <Navbar />
         <Hero />
         <About />
