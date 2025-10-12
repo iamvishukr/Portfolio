fd535b8c9f52a782d508728241777bddf45f37de
Vishal Kumar <140156661+iamvishukr@users.noreply.github.com>
2025-10-12 12:37:47 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,17 +4,18 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
-import resume from "../assets/resume.pdf"
+import resume from "../assets/resume.pdf";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-1 ">
-      <div className="flex flex-shrink-0 items-center ">
+    <nav className="mb-20 flex flex-wrap items-center justify-between py-1">
+      <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-32 hover:cursor-pointer" src={logo} alt="logo" />
       </div>
-      <div className="flex justify-center gap-4 text-2xl m-12 items-center">
+
+      <div className="flex flex-wrap justify-center gap-4 text-2xl m-4 items-center">
         <a href="https://www.linkedin.com/in/vishu23/">
-          <FaLinkedin className="hover:cursor-pointer " />
+          <FaLinkedin className="hover:cursor-pointer" />
         </a>
         <a href="https://github.com/iamvishukr">
           <FaGithub className="hover:cursor-pointer" />
@@ -25,13 +26,13 @@ const Navbar = () => {
         <a href="https://www.instagram.com/iamvishukr/">
           <FaInstagram className="hover:cursor-pointer" />
         </a>
-      <a
+        <a
           href={resume}
           download="Vishal_Resume.pdf"
           className="text-black border-2 py-2 px-6 focus:outline-none bg-yellow-500 border-yellow-600 rounded text-lg text-center w-full sm:w-auto"
         >
           Download Resume
-      </a>
+        </a>
       </div>
     </nav>
   );
